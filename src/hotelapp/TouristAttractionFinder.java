b634e392051badeafaf12e01a3ab3f9143656478
jovanimtzrico <jovanimtzrico@gmail.com>
2017-10-15 14:44:31 -0700

    Fix the bug in the fetchAttractions method in the TouristAttractionFinder class

--- src/hotelapp/TouristAttractionFinder.java
+++ src/hotelapp/TouristAttractionFinder.java
@@ -8,25 +8,26 @@ import org.json.simple.parser.ParseException;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
-import java.net.URL;
 import java.nio.charset.Charset;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class TouristAttractionFinder {
 
     private static final String host = "maps.googleapis.com";
     private static final String path = "/maps/api/place/textsearch/json";
-//    private static final String FIND_TYPE = "tourist+attractions+in+";
     private static final String FIND_TYPE = "tourist%20attractions+in+";
     private static final String API_KEY_PATH = "input/ApiKey.txt";
     private static final int PORT = 443;
     private static final int MILE_TO_METERS = 1609;
 
     private final ThreadSafeHotelData hdata;
-    private final Map<String,List<TouristAttraction>> listAttractions;
+    private final Map<String, List<TouristAttraction>> listAttractions;
 
-    /** Constructor for TouristAttractionFinder
+    /**
+     * Constructor for TouristAttractionFinder
      *
      * @param hdata
      */
@@ -43,33 +44,27 @@ public class TouristAttractionFinder {
      * get Attractions info. Adds attractions to the ThreadSafeHotelData.
      */
     public void fetchAttractions(int radiusInMiles) {
-        List<String> hotels= hdata.getHotels();
-        StringBuffer sb = new StringBuffer();
-        URL url;
+        List<String> hotels = hdata.getHotels();
         PrintWriter out = null;
         BufferedReader in = null;
         SSLSocket socket = null;
         try {
-            url = new URL("https://"+host);
-            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            socket = (SSLSocket) factory.createSocket(url.getHost(), PORT);
-            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-            for (String id: hotels){
-                String request = getRequest(id, radiusInMiles*MILE_TO_METERS);
+            for (String id : hotels) {
+                SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+                socket = (SSLSocket) factory.createSocket(host, PORT);
+                String request = getRequest(id, radiusInMiles * MILE_TO_METERS);
                 System.out.println("Request: " + request);
+                out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
                 out.println(request);
                 out.flush();
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String line;
+                StringBuffer sb = new StringBuffer();
                 while ((line = in.readLine()) != null) {
-                    if(!line.matches("^[A-Z].*")) {
+                    if (!line.matches("^[A-Z].*")) {
                         sb.append(line);
                         sb.append(System.lineSeparator());
                     }
-                }
-                if(id.equals("12539")){
-                    System.out.println(sb.toString());
                 }
                 loadAttractions(id, sb.toString());
             }
@@ -90,17 +85,17 @@ public class TouristAttractionFinder {
 
     /**
      * A method that creates a GET request for the given host and resource
+     *
      * @param radius
      * @return HTTP GET request returned as a string
      */
-    private  String getRequest(String hotelId, int radius) {
+    private String getRequest(String hotelId, int radius) {
         Object[] location = hdata.getLocationHotel(hotelId);
-        System.out.println(location[0] +" "+ location[1] + " "+ location[2]);
         String request = "GET " + path
-                + "?query=" + FIND_TYPE+((String) location[2]).replace(" ", "+")
-                + "&location="+ location[0] +","+ location[1]
+                + "?query=" + FIND_TYPE + ((String) location[2]).replace(" ", "+")
+                + "&location=" + location[0] + "," + location[1]
                 + "&radius=" + radius
-                + "&key="+ getApiKey()
+                + "&key=" + getApiKey()
                 + " HTTP/1.1" + System.lineSeparator()
                 + "Host: " + host + System.lineSeparator()
                 + "Connection: close" + System.lineSeparator()
@@ -122,14 +117,14 @@ public class TouristAttractionFinder {
             sb.append(", ");
             sb.append(hotelId);
             sb.append(System.lineSeparator());
-            attractions.forEach((attraction)->{
+            attractions.forEach((attraction) -> {
                 sb.append(attraction);
                 sb.append(System.lineSeparator());
             });
             sb.append("++++++++++++++++++++");
             sb.append(System.lineSeparator());
         });
-        sb.deleteCharAt(sb.length()-1);
+        sb.deleteCharAt(sb.length() - 1);
         try (PrintWriter writer = new PrintWriter(filename.toString())) {
             writer.println(sb.toString());
         } catch (IOException e) {
@@ -141,16 +136,16 @@ public class TouristAttractionFinder {
      * Get the Api Key
      * The method looks in a file called ApiKey
      * format of the file
-     *      key = ApIkEy
+     * key = ApIkEy
      *
      * @return the ApiKey String
      */
-    private static String getApiKey() {
+    private String getApiKey() {
         String key = "";
         Path path = Paths.get(API_KEY_PATH);
         try (BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"))) {
             String line;
-            while ((line=reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 key = line.replaceFirst(".*=", "");
             }
         } catch (IOException e) {
@@ -161,23 +156,23 @@ public class TouristAttractionFinder {
 
     /**
      * Load the attraction in the Map
-     * @param hotelId - Hotel id
+     *
+     * @param hotelId         - Hotel id
      * @param attractionsList - String JSON Object with all the attractions
      */
-    private void loadAttractions(String hotelId, String attractionsList){
+    private void loadAttractions(String hotelId, String attractionsList) {
         try {
             JSONParser parser = new JSONParser();
             Object obj = parser.parse(attractionsList);
             JSONArray attractions = (JSONArray) ((JSONObject) obj).get("results");
             Iterator<JSONObject> iterator = attractions.iterator();
             List<TouristAttraction> list = new ArrayList<>();
-            while(iterator.hasNext()){
+            while (iterator.hasNext()) {
                 double rating;
                 JSONObject att = iterator.next();
-                System.out.println((String) att.get("name"));
-                if(att.get("rating") == null) {
+                if (att.get("rating") == null) {
                     rating = 0;
-                }else if (att.get("rating").getClass() == Long.class){
+                } else if (att.get("rating").getClass() == Long.class) {
                     rating = ((Long) att.get("rating")).doubleValue();
                 } else {
                     rating = (Double) att.get("rating");
