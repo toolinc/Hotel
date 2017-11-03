5d722a4956bdca57d20efac2eda11dac4469a6de
edgar.martinez.rico@gmail.com <edgar.martinez.rico@gmail.com>
2017-11-03 10:45:34 -0700

    Fixing style

--- src/hotelapp/concurrent/ReentrantReadWriteLock.java
+++ src/hotelapp/concurrent/ReentrantReadWriteLock.java
@@ -1,9 +1,10 @@
 package hotelapp.concurrent;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A custom reentrant read/write lock that allows:
@@ -12,29 +13,18 @@ import org.apache.logging.log4j.Logger;
  * 3) A writer is allowed to acquire a read lock while holding the write lock.
  * 4) A writer is allowed to acquire another write lock while holding the write lock.
  * 5) A reader can not acquire a write lock while holding a read lock.
- *
+ * <p>
  * Use ReentrantReadWriteLockTest to test this class.
  * The code is modified from the code of Prof. Rollins.
  */
-public class ReentrantReadWriteLock {
-    private final Map<Long, Integer> readLocks;
-    private final Map<Long, Integer> writeLocks;
+public final class ReentrantReadWriteLock {
+
     private final static Logger log = LogManager.getRootLogger();
-
-
-    /**
-     * Constructor for ReentrantReadWriteLock
-     */
-    public ReentrantReadWriteLock() {
-        readLocks = new HashMap<>();
-        writeLocks = new HashMap<>();
-
-    }
+    private final Map<Long, Integer> readLocks = new HashMap<>();
+    private final Map<Long, Integer> writeLocks = new HashMap<>();
 
     /**
      * Return true if the current thread holds a read lock.
-     *
-     * @return true or false
      */
     public synchronized boolean isReadLockHeldByCurrentThread() {
         return readLocks.containsKey(Thread.currentThread().getId()); // do not forget to change
@@ -42,30 +32,25 @@ public class ReentrantReadWriteLock {
 
     /**
      * Return true if the current thread holds a write lock.
-     *
-     * @return true or false
      */
     public synchronized boolean isWriteLockHeldByCurrentThread() {
         return writeLocks.containsKey(Thread.currentThread().getId());
     }
 
     /**
-     * Non-blocking method that attempts to acquire the read lock. Returns true
-     * if successful.
-     * Checks conditions (whether it can acquire the read lock), and if they are true,
-     * updates readers info.
-     *
-     * Note that if conditions are false (can not acquire the read lock at the moment), this method
-     * does NOT wait, just returns false
-     * @return
+     * Non-blocking method that attempts to acquire the read lock. Returns true if successful. Checks conditions
+     * (whether it can acquire the read lock), and if they are true, updates readers info.
+     * <p>
+     * Note that if conditions are false (can not acquire the read lock at the moment), this method does NOT wait, just
+     * returns false.
      */
     public synchronized boolean tryAcquiringReadLock() {
-        if( writeLocks.isEmpty() || (writeLocks.size() == 1 && isWriteLockHeldByCurrentThread())){
-            if(isReadLockHeldByCurrentThread()) {
+        if (writeLocks.isEmpty() || (writeLocks.size() == 1 && isWriteLockHeldByCurrentThread())) {
+            if (isReadLockHeldByCurrentThread()) {
                 readLocks.put(Thread.currentThread().getId(), readLocks.get(Thread.currentThread().getId()) + 1);
             } else
                 readLocks.put(Thread.currentThread().getId(), 1);
-           return true;
+            return true;
         }
         return false;
     }
@@ -75,34 +60,29 @@ public class ReentrantReadWriteLock {
      * if successful.
      * Checks conditions (whether it can acquire the write lock), and if they are true,
      * updates writers info.
-     *
+     * <p>
      * Note that if conditions are false (can not acquire the write lock at the moment), this method
-     * does NOT wait, just returns false
-     *
-     * @return
+     * does NOT wait, just returns false.
      */
     public synchronized boolean tryAcquiringWriteLock() {
-        if(((writeLocks.size() ==1 && isWriteLockHeldByCurrentThread()) || writeLocks.isEmpty() ) &&readLocks.isEmpty()){
-            if(isWriteLockHeldByCurrentThread()){
-                writeLocks.put(Thread.currentThread().getId(), writeLocks.get(Thread.currentThread().getId())+1);
+        if (((writeLocks.size() == 1 && isWriteLockHeldByCurrentThread()) || writeLocks.isEmpty()) && readLocks.isEmpty()) {
+            if (isWriteLockHeldByCurrentThread()) {
+                writeLocks.put(Thread.currentThread().getId(), writeLocks.get(Thread.currentThread().getId()) + 1);
                 return true;
             } else {
-                writeLocks.put(Thread.currentThread().getId(),1);
+                writeLocks.put(Thread.currentThread().getId(), 1);
                 return true;
             }
         }
-       return false;
+        return false;
     }
 
     /**
-     * Blocking method that will return only when the read lock has been
-     * acquired.
-     * Calls tryAcquiringReadLock, and as long as it returns false, waits.
-     * Catches InterruptedException.
-     *
+     * Blocking method that will return only when the read lock has been acquired. Calls tryAcquiringReadLock, and as
+     * long as it returns false, waits. Catches InterruptedException.
      */
     public synchronized void lockRead() {
-        while(!tryAcquiringReadLock()){
+        while (!tryAcquiringReadLock()) {
             try {
                 this.wait();
             } catch (InterruptedException e) {
@@ -112,30 +92,28 @@ public class ReentrantReadWriteLock {
     }
 
     /**
-     * Releases the read lock held by the calling thread. Other threads might
-     * still be holding read locks. If no more readers after unlocking, calls notifyAll().
+     * Releases the read lock held by the calling thread. Other threads might still be holding read locks. If no more
+     * readers after unlocking, calls notifyAll().
      */
     public synchronized void unlockRead() {
         long currentThreadId = Thread.currentThread().getId();
-        if(isReadLockHeldByCurrentThread()){
-            if(readLocks.get(currentThreadId) == 1){
+        if (isReadLockHeldByCurrentThread()) {
+            if (readLocks.get(currentThreadId) == 1) {
                 readLocks.remove(currentThreadId);
-            }else {
+            } else {
                 readLocks.put(currentThreadId, readLocks.get(currentThreadId) - 1);
             }
         }
-        if(readLocks.isEmpty())
+        if (readLocks.isEmpty())
             notifyAll();
     }
 
     /**
-     * Blocking method that will return only when the write lock has been
-     * acquired.
-     * Calls tryAcquiringWriteLock, and as long as it returns false, waits.
-     * Catches InterruptedException.
+     * Blocking method that will return only when the write lock has been acquired. Calls tryAcquiringWriteLock, and as
+     * long as it returns false, waits. Catches InterruptedException.
      */
     public synchronized void lockWrite() {
-        while(!tryAcquiringWriteLock()){
+        while (!tryAcquiringWriteLock()) {
             try {
                 this.wait();
             } catch (InterruptedException e) {
@@ -146,21 +124,19 @@ public class ReentrantReadWriteLock {
     }
 
     /**
-     * Releases the write lock held by the calling thread. The calling thread
-     * may continue to hold a read lock.
-     * If the number of writers becomes 0, calls notifyAll.
+     * Releases the write lock held by the calling thread. The calling thread may continue to hold a read lock. If the
+     * number of writers becomes 0, calls notifyAll.
      */
-
     public synchronized void unlockWrite() {
         long currentThreadId = Thread.currentThread().getId();
-        if(isWriteLockHeldByCurrentThread()){
-            if(writeLocks.get(currentThreadId) == 1){
+        if (isWriteLockHeldByCurrentThread()) {
+            if (writeLocks.get(currentThreadId) == 1) {
                 writeLocks.remove(currentThreadId);
             } else {
                 writeLocks.put(currentThreadId, writeLocks.get(currentThreadId) - 1);
             }
         }
-        if(writeLocks.isEmpty())
+        if (writeLocks.isEmpty())
             notifyAll();
     }
 }
